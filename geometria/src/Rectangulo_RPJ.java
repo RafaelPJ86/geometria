8d6780c1564ee50c43238c29e0fd805b62404e6b
Rafael Piñol <r.pinyol.jarenyo@gmail.com>
2019-02-11 21:23:04 +0100

    version refactorizada

--- geometria/src/Rectangulo_RPJ.java
+++ geometria/src/Rectangulo_RPJ.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_RPJ extends FiguraGeometrica_RPJ {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_RPJ(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
